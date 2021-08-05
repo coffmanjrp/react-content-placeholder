ac2f3f415a463195e5afe1938558dc54f9c7ed21
coffmanjrp <coffmanjrp@gmail.com>
2021-08-04 19:18:39 -0700

    Fill html and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="card">
+        <div className="card-header animated-bg">&nbsp;</div>
+        <div className="card-content">
+          <h3 className="card-title animated-bg animated-bg-text">&nbsp;</h3>
+          <p className="card-excerpt">
+            &nbsp;
+            <span className="animated-bg animated-bg-text">&nbsp;</span>
+            <span className="animated-bg animated-bg-text">&nbsp;</span>
+            <span className="animated-bg animated-bg-text">&nbsp;</span>
+          </p>
+          <div className="author">
+            <div className="profile-img animated-bg">&nbsp;</div>
+            <div className="author-info">
+              <strong className="animated-bg animated-bg-text">&nbsp;</strong>
+              <small className="animated-bg animate-bg-text">&nbsp;</small>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
